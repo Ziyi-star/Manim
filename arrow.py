7a3bf4720cec5f00f458ddfc153c31ca6ceef307
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2024-11-28 15:19:48 +0100

    move arrow

--- arrow.py
+++ arrow.py
@@ -4,9 +4,12 @@ class Arrow(Scene):
     def construct(self):
         rect = Rectangle(color=WHITE, fill_opacity=0.5, height=2, width=2.5)
         circ = Circle().to_edge(DOWN)
-        #add tip
-        arrow = Line(start=rect.get_bottom(),end=circ.get_top()).add_tip()
-
+        #arrow follows
+        arrow = always_redraw(lambda: Line(start=rect.get_bottom(),end=circ.get_top()).add_tip())
+        #animation
         self.play(Create(VGroup(rect,circ,arrow)))
         self.wait()
+        # animate moving arrow
+        self.play(rect.animate.to_edge(UR),circ.animate.scale(0.5),run_time=2)
+
 
