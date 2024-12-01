19cedecc27e786e65f737cac20e8efa465eec5d1
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2024-12-01 17:55:10 +0100

    updates: rectangle moves with name tag together

--- scr/name_with_rectangle.py
+++ scr/name_with_rectangle.py
@@ -1,11 +1,16 @@
 from manim import *
 
-class NameWithRectangle(Scene):
+class NameWithRectangle_updaters(Scene):
     def construct(self):
         num = MathTex("ln(2)")
-        box = SurroundingRectangle(num,color=BLUE, fill_opacity=0.5, fill_color=RED, buff=2)
-        name = Tex("Ziyi Liu").next_to(box, DOWN, buff=1)
+        #box und name zusammenbewegen
+        box = always_redraw(lambda: 
+        SurroundingRectangle(num,color=BLUE, fill_opacity=0.5, fill_color=RED, buff=2)
+        )
 
-        self.play(Create(VGroup(num,box,name)), run_time = 3)
+        name = always_redraw(lambda:Tex("Ziyi Liu").next_to(box, DOWN, buff=1))
+
+        self.play(Create(VGroup(num,box,name)))
+        self.play(num.animate.shift(RIGHT*2), run_time = 2)
         self.wait()
 
