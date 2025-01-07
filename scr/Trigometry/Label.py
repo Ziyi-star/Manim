a7bc56db5674fbd8ae861c90f7cbf8096a808c87
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2025-01-07 14:39:30 +0100

    Update

--- scr/Trigometry/Label.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-
-class LabelSinus(Scene):
-    def construct(self):
-        text = Text("Sinusfunktion", font="Arial", font_size=36, color=YELLOW)
-
-        self.play(Write(text))
-        self.wait(2)
