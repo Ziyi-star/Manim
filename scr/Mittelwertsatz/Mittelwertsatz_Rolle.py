68fa5e2f7629e266837c1259d22c7bd81544d407
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2025-02-10 12:38:34 +0100

    secant finished

--- scr/Mittelwertsatz/Mittelwertsatz_Rolle.py
+++ scr/Mittelwertsatz/Mittelwertsatz_Rolle.py
@@ -34,21 +34,37 @@ class MittelwertsatzRolle(Scene):
         graph_label = axes.get_graph_label(graph, label="f(x)")
 
         self.play(Create(grid), Create(axes), Create(x_label), Create(y_label), Create(graph), Create(graph_label))
-        self.wait(2)
+        #self.wait(2)
         
         #todo: draw the tangent line at point x = 4.5 of function f(x)
-        x0 = 3.6
-        fx0 = func(x0)
-        x0_fx0_dot = Dot(axes.coords_to_point(x0, fx0),color=RED)
-        # Derivative of the function
-        def func_derivative(x):
-            h = 1e-5
-            return (func(x + h) - func(x)) / h
-        slope = func_derivative(x0)
-        tangent_line = axes.plot(lambda x: slope * (x - x0) + fx0, color=RED, x_range=[2.7, 4.5])
+        def draw_tangent_line_at(x0):
+            fx0 = func(x0)
+            x0_fx0_dot = Dot(axes.coords_to_point(x0, fx0), color=RED)
+            # Derivative of the function
+            def func_derivative(x):
+                h = 1e-5
+                return (func(x + h) - func(x)) / h
+            slope = func_derivative(x0)
+            tangent_line = axes.plot(lambda x: slope * (x - x0) + fx0, color=RED, x_range=[x0 - 1, x0 + 1])
+            return x0_fx0_dot, tangent_line
 
-        self.play(Create(x0_fx0_dot))
-        self.play(Create(tangent_line))
+        #todo: draw the secant line at point x = 0.5 and x = 4.5 of function f(x)
+        def draw_secant_line_at(a, b):
+            fx0 = func(a)
+            fx1 = func(b)
+            x0_fx0_dot = Dot(axes.coords_to_point(a, fx0), color=RED)
+            x1_fx1_dot = Dot(axes.coords_to_point(b, fx1), color=RED)
+            secant_line = axes.plot(lambda x: (fx1 - fx0) / (b - a) * (x - a) + fx0, color=GREEN, x_range=[a - 1, b + 1])
+            return x0_fx0_dot, x1_fx1_dot, secant_line
+        
+        xa_fxa_dot, xb_fxb_dot, secant_line = draw_secant_line_at(0.7, 4.5)
+        self.play(Create(xa_fxa_dot), Create(xb_fxb_dot), Create(secant_line))
+        self.wait(2)
+
+         # Draw the tangent line at point x = 3,6 
+        x0_fx0_dot_two, tangent_line_two = draw_tangent_line_at(3.6)
+        self.play(Create(x0_fx0_dot_two))
+        self.play(Create(tangent_line_two))
         self.wait(2)
 
 
