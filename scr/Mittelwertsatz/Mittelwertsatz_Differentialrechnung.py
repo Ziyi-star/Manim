76637ea03e424cfbde58b8967719bcda0a04a9f5
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2025-02-18 10:21:35 +0100

    Refactor animation code and add ValueTracker for tangent line movement

--- scr/Mittelwertsatz/Mittelwertsatz_Differentialrechnung.py
+++ scr/Mittelwertsatz/Mittelwertsatz_Differentialrechnung.py
@@ -53,8 +53,7 @@ class MittelwertsatzDifferentialrechnung(Scene):
             secant_line = axes.plot(lambda x: (fx1 - fx0) / (b - a) * (x - a) + fx0, color=GREEN, x_range=[a - 1, b + 1])
             return x0_fx0_dot, x1_fx1_dot, secant_line
 
-        
-        
+
         # Animation
         self.play(Create(grid))
         self.wait(1)
@@ -76,39 +75,51 @@ class MittelwertsatzDifferentialrechnung(Scene):
         dashline_fb = DashedLine(axes.coords_to_point(b, 0), axes.coords_to_point(b, f_b), color=ORANGE)
         self.play(Create(dashline_fa), run_time = 1)
         self.play(Create(dashline_fb), run_time = 1)
-        self.wait(1)
+        #self.wait(1)
         fa_label = MathTex("f(a)").next_to(axes.coords_to_point(a, f_a), LEFT).scale(0.7)
         fb_label = MathTex("f(b)").next_to(axes.coords_to_point(b, f_b), RIGHT).scale(0.7)
         self.play(Create(fa_label))
         self.play(Create(fb_label))
-        self.wait(1)
+        #self.wait(1)
 
         xa_fxa_dot, xb_fxb_dot, secant_line = draw_secant_line_at(0.7, 4.5)
         self.play(Create(xa_fxa_dot), Create(xb_fxb_dot))
-        self.wait(1)
+        #self.wait(1)
         self.play(Create(secant_line))
-        self.wait(1)
+        #self.wait(1)
         sekante_label = MathTex("Sekante").next_to(secant_line.get_center(), UP).scale(0.7)
         self.play(Create(sekante_label))
+        #self.wait(1)
+
+        #  # Draw the tangent line at x = 3.6
+        # c = 3.6
+        # f_c = func(c)
+        # c_label = MathTex("c").next_to(axes.coords_to_point(c, 0), DOWN).set_color(GREEN)
+        # self.play(Create(c_label))
+        # dashline_fc = DashedLine(axes.coords_to_point(c, 0), axes.coords_to_point(c, f_c), color=GREEN)
+        # self.play(Create(dashline_fc), run_time = 1)
+        # x0_fx0_dot, tangent_line = draw_tangent_line_at(c)
+        # self.play(Create(x0_fx0_dot))
+        # self.play(Create(tangent_line))
+        # tangent_line_label = Tex("Tangente bei c").next_to(tangent_line.get_center(), DOWN).scale(0.7).set_color(GREEN)
+        # self.play(Create(tangent_line_label))
+        # self.wait(2)
+        # derivative_formula = MathTex(r"f'(c) = \frac{f(b) - f(a)}{b - a}", color=WHITE).to_edge(RIGHT).scale(0.7)
+        # self.play(Create(derivative_formula), run_time = 2)
+        # self.wait(2)
+
+        
+        # Create a ValueTracker for the moving endpoint t (starting at b)
+        slope_tracker = ValueTracker(a)
+        
+        #slope and Dot
+        tangent_group = always_redraw(
+            lambda: VGroup(*draw_tangent_line_at(slope_tracker.get_value()))
+        )        
+        # Add these to the scene and animate t_tracker from a to b
+        self.add(tangent_group)
+        self.play(slope_tracker.animate.set_value(b), run_time=10, rate_func=linear)
         self.wait(1)
-
-         # Draw the tangent line at x = 3.6
-        c = 3.6
-        f_c = func(c)
-        c_label = MathTex("c").next_to(axes.coords_to_point(c, 0), DOWN).set_color(GREEN)
-        self.play(Create(c_label))
-        dashline_fc = DashedLine(axes.coords_to_point(c, 0), axes.coords_to_point(c, f_c), color=GREEN)
-        self.play(Create(dashline_fc), run_time = 1)
-        x0_fx0_dot, tangent_line = draw_tangent_line_at(c)
-        self.play(Create(x0_fx0_dot))
-        self.play(Create(tangent_line))
-        tangent_line_label = Tex("Tangente bei c").next_to(tangent_line.get_center(), DOWN).scale(0.7).set_color(GREEN)
-        self.play(Create(tangent_line_label))
-        self.wait(2)
-        derivative_formula = MathTex(r"f'(c) = \frac{f(b) - f(a)}{b - a}", color=WHITE).to_edge(RIGHT).scale(0.7)
-        self.play(Create(derivative_formula), run_time = 2)
-        self.wait(2)
-
 
 
 
