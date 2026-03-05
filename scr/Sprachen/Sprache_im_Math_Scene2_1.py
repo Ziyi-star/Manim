6fc84f7c4c6c273891359e77fb94ffc481443e4c
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2026-03-05 14:30:10 +0100

    update for pause

--- scr/Sprachen/Sprache_im_Math_Scene2_1.py
+++ scr/Sprachen/Sprache_im_Math_Scene2_1.py
@@ -1,6 +1,5 @@
 from manim import *
 
-
 class Sprache_S2_1(Scene): 
 
     def construct(self):
@@ -11,6 +10,8 @@ class Sprache_S2_1(Scene):
         c_alltag = ManimColor("#8c0f3d")
         c_bildung = ManimColor("#5a215c")
         c_fach = ManimColor("#00205b")
+        c_arrow = ManimColor("#0996d7")
+        c_card = ManimColor("#f2f2f2")
 
         # Geometrie
         h = 1.4  # Höhe des Strahls
@@ -71,91 +72,33 @@ class Sprache_S2_1(Scene):
         strahl = VGroup(seg1, seg2, seg3, div1_a, div1_b, div2_a, div2_b, t1, t2, t3)
         strahl.move_to(ORIGIN)
 
+
+        box = RoundedRectangle(
+            corner_radius=0.16,
+            width=4,
+            height=1.2,
+            fill_color=LIGHTER_GRAY,
+            fill_opacity=1,
+            stroke_color=DARK_GRAY,
+            stroke_width=2,
+        )
+        label = Tex(r"Fachliche\\Lerngelegenheit", color=BLACK).scale(0.7).move_to(box.get_center())
+        card = VGroup(box, label).move_to(UP * 2.5)  # top row position
+
         # ---------- Animation (nacheinander) ----------
-        self.play(FadeIn(seg1), FadeIn(t1), run_time=0.9)
-        self.wait(0.5)
+        self.play(FadeIn(box), FadeIn(label), run_time=1)
 
-        self.play(FadeIn(seg2), Create(div1_a), Create(div1_b), FadeIn(t2), run_time=0.9)
-        self.wait(0.5)
+        self.play(FadeIn(seg1), FadeIn(t1), run_time=1)
+        self.wait(1)
 
-        self.play(FadeIn(seg3), Create(div2_a), Create(div2_b), FadeIn(t3), run_time=0.9)
-        self.wait(3.5)
+        self.play(FadeIn(seg2), Create(div1_a), Create(div1_b), FadeIn(t2), run_time=1)
+        self.wait(1)
 
-        # -----------------------------
-        # Sprechblasen als PNGs + Text
-        # -----------------------------
+        self.play(FadeIn(seg3), Create(div2_a), Create(div2_b), FadeIn(t3), run_time=1)
+        self.wait(4)
 
-        # Ankerpunkte (wo die Blasen "rauswachsen" sollen)
-        anchor_alltag = seg1.get_top() + LEFT * 1.0
-        anchor_fach = seg3.get_top() + RIGHT * 0.6
 
-        # --- Blase 1: Alltagssprache ---
-        bubble1_img = ImageMobject("media/images/speechBubble_alltag.png").set_z_index(5)
-        bubble1_img.scale(0.425)
-        bubble1_img.next_to(seg1, UP, buff=0.1).shift(RIGHT * 0.5)
 
-        bubble1_text = Tex(
-            r"``Wenn die Ableitung",
-            r"positiv ist, geht die Kurve",
-            r"nach oben''",
-            color=WHITE
-        ).scale(0.6)
-        bubble1_text.arrange(DOWN, buff=0.15)
-        bubble1_text.move_to(bubble1_img.get_center()).shift(UP * 0.25).set_z_index(6)
-
-        bubble1 = Group(bubble1_img, bubble1_text)
-
-        self.play(GrowFromPoint(bubble1_img, anchor_alltag), run_time=0.9)
-        self.play(FadeIn(bubble1_text), run_time=0.3)
-        self.wait(2.3)
-
-        # --- Blase 2: Fachsprache ---
-        bubble2_img = ImageMobject("media/images/SpeechBubble_fach.png").set_z_index(5)
-        bubble2_img.scale(0.425)
-        bubble2_img.next_to(seg3, DOWN, buff=0.1).shift(RIGHT * 0.5)
-
-        bubble2_text = Tex(
-            r"`` Wenn $f'(x_0) > 0$ gilt,",
-            r" ist der Graph von, $f$ in einer ",
-            r"Umgebung von $x_0$ streng",
-            r" monoton steigend.''",
-            color=WHITE
-        ).scale(0.6)
-        bubble2_text.arrange(DOWN, buff=0.135)
-        bubble2_text.move_to(bubble2_img.get_center()).shift(DOWN * 0.35 + RIGHT * 0.08).set_z_index(6)
-
-        bubble2 = Group(bubble2_img, bubble2_text)
-        # -----------------------------
-        # Sammelgruppe für späteres Ausblenden
-        # -----------------------------
-        strahl_gruppe = Group(
-            seg1, seg2, seg3,
-            div1_a, div1_b,
-            div2_a, div2_b,
-            t1, t2, t3
-        )
-
-        sprechblasen_gruppe = Group(
-            bubble1,
-            bubble2
-        )
-
-        self.play(GrowFromPoint(bubble2_img, anchor_fach), run_time=0.9)
-        self.play(FadeIn(bubble2_text), run_time=0.3)
-        self.wait(2.5)
-
-        # -----------------------------
-        # Szene aufräumen für nächste Inhalte
-        # -----------------------------
-        self.wait(0.8)
-
-        self.play(
-            FadeOut(strahl_gruppe),
-            FadeOut(sprechblasen_gruppe),
-            run_time=1.4
-        )
-
-        self.wait(2)
 
        
 
