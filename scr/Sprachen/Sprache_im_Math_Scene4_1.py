83c877838e3a95c28beb89df15a28d2e9df9e3b8
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2026-03-17 20:39:07 +0100

    fertig!!

--- scr/Sprachen/Sprache_im_Math_Scene4_1.py
+++ scr/Sprachen/Sprache_im_Math_Scene4_1.py
@@ -15,14 +15,38 @@ class Sprache_S4_1(Scene):
         circus = ImageMobject(CIRCUS).shift(LEFT * 2)
         charts = ImageMobject(CHARTS).shift(LEFT * 2 + DOWN * 1.5)
 
-        self.add(circus, charts)
-
-        # todo: add texts around circle
+        # Add texts around circle
         label_enaktiv = Text("Enaktiv", color=BLACK, font_size=20).shift(LEFT * 6 + UP * 1)
         label_symbolisch = Text("Symbolisch", color=BLACK, font_size=20).shift(RIGHT * 2 + UP * 1)
         label_ikonisch = Text("Ikonisch", color=BLACK, font_size=20).shift(LEFT * 2 + DOWN * 3.5)
 
-        self.add(label_enaktiv, label_ikonisch, label_symbolisch)
+
+        # Add book in the middle and text "EIS-Prinzip(Bruder)?" in a rectangle around it
+        book = ImageMobject(BOOK).shift(RIGHT*1.5 + DOWN*1.3)
+        eis_text = Text("EIS-Prinzip (Bruner)", color=BLACK, font_size=30)
+        box = SurroundingRectangle(
+            eis_text,
+            color="#d0d0d0",
+            fill_color="#d0d0d0",
+            fill_opacity=1,
+            buff=0.25,
+            stroke_width=0,
+            corner_radius=0.1
+        )
+        title_group = VGroup(box, eis_text)
+        title_group.move_to(RIGHT * 4 + DOWN * 2)
+
+        # Animations
+        self.play(FadeIn(book), FadeIn(title_group), run_time=1)
+        self.wait(1)
+        self.play(FadeIn(circus), FadeIn(charts), run_time=1)
+        self.wait(1)
+        self.play(Write(label_enaktiv), run_time=1)
+        self.play(Write(label_symbolisch), run_time=1)
+        self.play(Write(label_ikonisch), run_time=1)
+        self.wait(2)
+
+
         
 
 
