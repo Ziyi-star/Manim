ce53126f57d5758b132d8a4cd0840297bf08ed0b
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2026-03-17 19:06:29 +0100

    circles fertig

--- scr/Sprachen/Sprache_im_Math_Scene4_1.py
+++ scr/Sprachen/Sprache_im_Math_Scene4_1.py
@@ -10,5 +10,21 @@ class Sprache_S4_1(Scene):
         CHARTS = f"{IMG_DIR}/charts.png"
         CIRCUS = f"{IMG_DIR}/circus.png"
         BOOK = f"{IMG_DIR}/book.png"
-    
+
+        # Add image and put them in the right position
+        circus = ImageMobject(CIRCUS).shift(LEFT * 2)
+        charts = ImageMobject(CHARTS).shift(LEFT * 2 + DOWN * 1.5)
+
+        self.add(circus, charts)
+
+        # todo: add texts around circle
+        label_enaktiv = Text("Enaktiv", color=BLACK, font_size=20).shift(LEFT * 6 + UP * 1)
+        label_symbolisch = Text("Symbolisch", color=BLACK, font_size=20).shift(RIGHT * 2 + UP * 1)
+        label_ikonisch = Text("Ikonisch", color=BLACK, font_size=20).shift(LEFT * 2 + DOWN * 3.5)
+
+        self.add(label_enaktiv, label_ikonisch, label_symbolisch)
+        
+
+
+
 
